1ad1427f28f12a05606efd986c41a22133b09eaf
Alex <yoyobq@hotmail.com>
2026-05-04 02:10:11 +0900

    refactor(admin-user-list): 移除不必要的类型导出
    - 删除了与管理员用户列表相关的类型导出

--- src/features/admin-user-list/index.ts
+++ src/features/admin-user-list/index.ts
@@ -1,10 +1,4 @@
 export {
-  type AdminUserListItem,
-  type AdminUserListQuery,
-  type AdminUserListResult,
-  DEFAULT_ADMIN_USER_LIST_QUERY,
-} from './application/get-admin-users';
-export {
   requestAdminUserAccountStatusUpdate,
   requestAdminUserStaffEmploymentStatusUpdate,
 } from './infrastructure/admin-user-list-api';
